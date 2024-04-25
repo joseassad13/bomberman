a93f42262589c48f67eac326c9b1f281458cc866
joseassad13 <159552383+joseassad13@users.noreply.github.com>
2024-04-25 00:59:45 +0000

    Version 3

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -3,15 +3,30 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <list>
+#include <fstream>
 
 using namespace std;
 using namespace ftxui;
 int main(int argc, char const *argv[])
 {
+    list<string> textos;
+    fstream imagen;
+    imagen.open("./assets/imagen.txt");
+
+    string linea;
+    while (getline(imagen, linea))
+    {
+        textos.push_back(linea);
+    }
+    imagen.close();
+
     /*const string texto = "Hola mundo";
     Element textElement = text(texto);*/
     int fotograma = 0;
     string reset;
+    int posX = 0;
+    int posY = 6;
 
     while (true)
     {
@@ -23,12 +38,32 @@ int main(int argc, char const *argv[])
 
         // Element dibujo = border(hbox(cuadro));
 
-        Dimensions Alto = Dimension::Fixed(10);
+        Dimensions Alto = Dimension::Full();
         Dimensions Ancho = Dimension::Full();
 
         Screen pantalla = Screen::Create(Ancho, Alto);
 
         Render(pantalla, dibujo);
+
+        /*textos.push_back("Primera linea");
+        textos.push_back("Segunda linea");
+        textos.push_back("Tercera linea");*/
+        int l = 0;
+        for (auto &&texto : textos)
+        {
+            int i = 0;
+            for (auto &&letra : texto)
+            {
+                pantalla.PixelAt(posX + i, posY + l).character = letra;
+                i++;
+            }
+            l++;
+        }
+
+        posX++;
+        /*pantalla.PixelAt(posX+1,6).character = "O";
+        pantalla.PixelAt(posX+2,6).character = "L";
+        pantalla.PixelAt(posX+3,6).character = "A";*/
 
         pantalla.Print();
         reset = pantalla.ResetPosition();
