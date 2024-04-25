088de5ef27e24cc7cbfa17ec03ea7dab7b0fcdaf
joseassad13 <159552383+joseassad13@users.noreply.github.com>
2024-04-25 02:02:24 +0000

    Version 4

---
+++ include/pintor/Vector.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <list>
+#include <string>
+using namespace std;
+
+class Vector
+{
+private:
+    int posX;
+    int posY;
+
+public:
+    Vector() {}
+    ~Vector() {}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 int main(int argc, char const *argv[])
 {
+    Juego j;
+    j.iniciar;
     std::cout << "Bomberman" << std::endl;
     return 0;
 }
